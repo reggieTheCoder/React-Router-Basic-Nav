c908062f0ae158c9c4d0a1ae65eac7e0b5df30e6
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-02-12 21:57:36 -0500

    Inside of your App.js file import { Route } from 'react-router-dom';

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+// Inside of your App.js file import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = () => (
   <div>
