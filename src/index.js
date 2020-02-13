c5ec644fa2e98c0779ba0d1c2718a77f90785c58
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-02-12 21:49:09 -0500

    import Browser Router as Router

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+// import your BrowserRouter as Router inside your index.js file.
+import { BrowserRouter as Router} from 'react-router-dom';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
