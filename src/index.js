e99c0e20fe01c12517e5493ed3649dd7d1b8fdb3
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-02-12 21:53:23 -0500

    Wrap your <App /> component that you're passing to ReactDOM.render() with your new Router component.

--- src/index.js
+++ src/index.js
@@ -6,5 +6,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 // import your BrowserRouter as Router inside your index.js file.
 import { BrowserRouter as Router} from 'react-router-dom';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// Wrap your <App /> component that you're passing to ReactDOM.render() with your new Router component.
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>
+    ,document.getElementById('root'));
